b1e4df6a728daea320eb0a3494266923892a3a9b
shaypachter <shaypachter@gmail.com>
2026-04-11 07:31:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -976,7 +976,6 @@ with tab4:
         total_cost=('total_cost','sum'),
         requests=('requests','sum')
     ).reset_index()
-    eff_agg = eff_agg[eff_agg['requests'] >= 500]
     eff_agg['cost_per_req'] = eff_agg['total_cost'] / eff_agg['requests']
 
     fig_eff = go.Figure()
